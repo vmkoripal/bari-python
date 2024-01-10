87894cc4b22bab2dc930b613a51273cc6931f162
vkoripal <vkoripal@gmail.com>
2024-01-10 14:30:49 -0800

    add

---
+++ areaofcircle.py
@@ -0,0 +1,9 @@
+import math
+
+radius = float(input("Enter Radius"))
+
+area = math.pi * radius ** 2 
+
+print ("Area is ", area)
+
+
---
+++ tsaofcuboid.py
@@ -0,0 +1,11 @@
+length = float(input("Enter Length"))
+
+breadth = float(input("Enter Breadth"))
+
+height = float(input("Enter Height"))
+
+area = 2 * (length * height + length * breadth + breadth * height) 
+
+print ("Total Surface area of cubloid is ", area)
+
+
