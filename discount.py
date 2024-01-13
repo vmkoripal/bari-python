09dd1a0b4ecf435e5461b7113528f22ab33e8d3a
vkoripal <vkoripal@gmail.com>
2024-01-12 18:37:02 -0800

    discount

---
+++ discount.py
@@ -0,0 +1,15 @@
+amount = int(input("Enter Amount"))
+
+if amount <= 1000:
+    discount = amount * 10 / 100
+elif amount > 1000 and amount <= 5000:
+    discount = amount * 20 / 100
+elif amount > 5000 and amount <= 10000:
+    discount = amount * 30 / 100
+else:
+    discount = amount * 50 / 100
+discamount = amount - discount
+
+print("pay", discamount)
+
+   
